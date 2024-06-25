7c7a09b3e264072f1c3c7cbd0c887528590ec97e
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-06-26 02:26:21 +0300

    sigma

---
+++ Dark/assets/user/js/album_images.js
@@ -0,0 +1,65 @@
+var albumImages = [
+    {
+        album: [
+            { created_at: "Saved on 24/09/2017 21:02", ss: "6437256809371611639" },
+        ],
+        name: "The Legend of Zelda: Breath of the Wild",
+        id: "6437256809225675698",
+    },
+    {
+        album: [
+            { created_at: "Saved on 19/09/2017 18:05", ss: "6437256809368913199" },
+        ],
+        name: "Super Mario™ 64",
+        id: "14866558073615253628",
+    },
+    {
+        album: [
+            { created_at: "Saved on 27/07/2017 10:27", ss: "6437256809329844359" },
+        ],
+        name: "Splatoon™",
+        id: "14866558073673370352",
+    },
+    {
+        album: [
+            { created_at: "Saved on 02/02/2017 16:35", ss: "6437256809205422037" },
+        ],
+        name: "The Legend of Zelda: The Wind Waker HD",
+        id: "14866558073070242120",
+    },
+    {
+        album: [
+            { created_at: "Saved on 26/11/2016 15:11", ss: "6437256809150251347" },
+        ],
+        name: "Mario Kart 8",
+        id: "14866558073268532636",
+    },
+    { album: [
+        { created_at: "Saved on 20/11/2016 17:10", ss: "6437256809145700254" }
+    ], 
+    name: "Nintendo Land", 
+    id: "14866558072985245726" },
+    { album: [
+        { created_at: "Saved on 03/10/2016 20:32", ss: "6437256809109760424" }], name: "Hyrule Warriors", id: "14866558073424953781" },
+    {
+        album: [
+            { created_at: "Saved on 23/09/2016 15:53", ss: "6437256809101627936" },
+        ],
+        name: "LEGO® STAR WARS™: The Force Awakens",
+        id: "6437256809016967254",
+    },
+    {
+        album: [
+            { created_at: "Saved on 28/08/2016 08:52", ss: "6437256809079027163" },
+        ],
+        name: "The Legend of Zelda: A Link Between Worlds",
+        id: "14866558073093301778",
+    },
+    {
+        album: [
+            { created_at: "Saved on 30/06/2016 21:04", ss: "6437256809019930359" },
+        ],
+        name: "The Legend of Zelda: Majora's Mask 3D",
+        id: "14866558073568523354",
+    },
+];
---
+++ Dark/assets/user/js/post_index.js
@@ -0,0 +1,4 @@
+var post_index = [
+{"file_name":"./assets/user/js/posts/2024.js","var_name":"posts2024"},
+{"file_name":"./assets/user/js/posts/2025.js","var_name":"posts2025"},
+];
